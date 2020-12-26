2637f095e770bee42b528b75bbabc95101297cd0
KishanItaliya <kishanpitaliya@gmail.com>
2020-12-26 15:15:04 +0530

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function login(Request $req){
+        $user = User::where(['email' => $req->email])->first();
+        if($user || Hash::check($req->password,$user->password)){
+            $req->session()->put('user',$user);
+            return redirect('/');
+        }
+        else{
+            return "Username or Password is not matched";
+        }
+    }
+}
